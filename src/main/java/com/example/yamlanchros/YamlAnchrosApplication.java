31ac6092ded12472e5b2dfe6ec8f9fcbf3f65ba9
rthakur <rthakur@pandora.com>
2020-03-04 11:55:56 -0800

    Initial commit

---
+++ src/main/java/com/example/yamlanchros/YamlAnchrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.yamlanchros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlAnchrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlAnchrosApplication.class, args);
+	}
+
+}
